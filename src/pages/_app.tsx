39843a1205e3f81693569cb0b8f3b3bb35abcb8b
flektor <gdrosin@gmail.com>
2023-06-30 14:52:59 +0200

    modified landing page, added dotsloader styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
+import "~/components/DotsLoader/index.css";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
