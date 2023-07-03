b114cdf1da37d496194ecdc058e016054e8897df
flektor <gdrosin@gmail.com>
2023-07-03 22:26:54 +0200

    changed register button so it subscribes for incoming quick match events

--- src/server/api/routers/registrations.ts
+++ src/server/api/routers/registrations.ts
@@ -1,14 +1,28 @@
 import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
+import { pusherSend } from "~/server/utils";
 
 export const registrationsRouter = createTRPCRouter({
   add: protectedProcedure
     .input(z.object({ activityId: z.string() }))
-    .mutation(({ input, ctx }) => {
+    .mutation(async ({ input, ctx }) => {
+      const user = await ctx.prisma.registrations.findFirst({
+        where: { activityId: input.activityId },
+      });
+
+      if (user) {
+        pusherSend({
+          receivers: `user-${user.userId}`,
+          slug: `quick-${input.activityId}`,
+          action: "quick",
+        });
+      }
+
       const data = {
         userId: ctx.session.user.id,
         activityId: input.activityId,
       };
+
       return ctx.prisma.registrations.create({ data });
     }),
 
