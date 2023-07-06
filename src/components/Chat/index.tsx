f8a8a51dcc79e3f51001ec3b6b2776427aaeb27d
flektor <gdrosin@gmail.com>
2023-07-06 03:17:20 +0200

    made chat bit prittier

--- src/components/Chat/index.tsx
+++ src/components/Chat/index.tsx
@@ -2,7 +2,7 @@ import React, {
   type FormEvent,
   FunctionComponent,
   useEffect,
-  useState,
+  useRef,
 } from "react";
 import { z } from "zod";
 import { api } from "../../utils/api";
@@ -21,21 +21,30 @@ export const Chat: FunctionComponent<
   { channel, isLoading, update },
 ) => {
   const { data: session } = useSession();
+  const formRef = useRef<HTMLFormElement>(null);
+  const messagesListRef = useRef<HTMLUListElement>(null);
 
   function getUserNameById(userId: string) {
     return channel.users.find((user) => user.userId === userId)?.name || "user";
   }
 
   function scrollToBottom() {
-    // this.messagesEnd.scrollIntoView({ behavior: "smooth" });
+    if (!messagesListRef.current) return;
+    const lastListItem = messagesListRef.current.lastElementChild;
+
+    if (lastListItem) {
+      lastListItem.scrollIntoView({ behavior: "smooth", block: "end" });
+    }
   }
+
+  useEffect(() => scrollToBottom(), [channel.messages]);
 
   const { mutate: sendMessage } = api.chat.sendMessage.useMutation({
     onError: (error) => console.log(error),
     onSuccess: (message) => {
       message.sentBy = getUserNameById(message.sentBy);
       channel.messages.push(message);
-      scrollToBottom();
+      formRef.current?.reset();
     },
   });
 
@@ -68,17 +77,20 @@ export const Chat: FunctionComponent<
   return (
     <section
       aria-label="chat"
-      className="p-2 max-w-xl h-screen overflow-y-auto	overflow-x-hidden "
+      className="w-full md:w-3/6 lg:w-2/3  "
     >
-      <div className="h-2/6">
-        <div className="flex justify-center w-full">
-          <h2 className="text-2xl text-white">Chat</h2>
-        </div>
-        <hr className="h-px border-0 bg-gradient-to-r from-#0000000 via-[#cc66ff] to-#0000000" />
-
+      <div className="flex justify-center w-full">
+        <h2 className="text-2xl text-white/50">Chat</h2>
+      </div>
+      <hr className="h-px ml-2 mr-2 border-0 bg-gradient-to-r from-#0000000 via-[#cc66ff] to-#0000000" />
+      <div
+        id="message-scroll-container"
+        className="h-2/6 w-full md:w-[50vw] max-h-[50vh] overflow-y-auto	overflow-x-hidden rounded-t-xl h-80 scrollbar-thin scrollbar-thumb-[#cc66ff] scrollbar-track-gray-100"
+      >
         <ul
+          ref={messagesListRef}
           aria-label="messages"
-          className="flex-col  w-full"
+          className="w-full bg-black/20 p-2 mx-auto "
         >
           {isLoading && <Spinner />}
 
@@ -89,60 +101,54 @@ export const Chat: FunctionComponent<
               : "[#cc66ff]";
 
             const float = message.sentBy === username
-              ? "justify-end"
-              : "justify-start";
+              ? "justify-self-end"
+              : "justify-self-start";
             return (
-              <li
-                key={index}
-                className={`flex m-2 max-w-lg flex-col gap-4 rounded-xl bg-${msg_clr} p-4 text-white ${float}`}
-              >
-                <i>
-                  <span>{message.sentBy}</span>{" "}
-                  <span className="text-[#cc66ff]">
-                    {message.sentAt.toLocaleTimeString()}
-                  </span>
-                </i>
-                <p>{message.content}</p>
-                <div className="flex justify-center gap-3">
-                  <button
-                    className={`rounded-full p-1 w-10 h-10 font-semibold text-white no-underline transition border-2 border-${rct_clr} bg-black/20 hover:bg-black/10 hover:border-white hover:text-white`}
-                  >
-                    👍
-                  </button>
-                  <button
-                    className={`rounded-full p-1 w-10 h-10 font-semibold text-white no-underline transition border-2 border-${rct_clr} bg-black/20 hover:bg-black/10 hover:border-white hover:text-white`}
-                  >
-                    👎
-                  </button>
+              <li key={index}>
+                <div
+                  className={`m-2 rounded-lg bg-${msg_clr} p-4 text-white w-fit break-words ${float} ${
+                    message.sentBy === username
+                      ? "ml-auto border border-white/20  bg-white/10"
+                      : "mr-auto border border-white/20  bg-white/5"
+                  }`}
+                >
+                  <div className="flex items-center">
+                    <span className="text-white/60">{message.sentBy}</span>
+                    <i className="pl-3 text-xs text-[#cc66ff]">
+                      {message.sentAt.toLocaleTimeString()}
+                    </i>
+                  </div>
+                  <p className="whitespace-normal break-words">
+                    {message.content}
+                  </p>
                 </div>
               </li>
             );
           })}
         </ul>
-
-        <hr className="h-px border-0 bg-gradient-to-r from-#0000000 via-[#cc66ff] to-#0000000" />
-
-        <form
-          onSubmit={onSubmitHandler}
-          className="flex max-w-md flex-col gap-4 rounded-xl bg-white/10 p-4 text-white"
-        >
-          <textarea
-            name="content"
-            aria-label="message content input"
-            className="p-2 rounded-lg text-lg bg-gradient-to-b from-[#25213C] to-[#1b1b2e]"
-            rows={2}
-            required
-          />
-          <div className="flex justify-center">
-            <button
-              type="submit"
-              className="rounded-full px-10 py-3 font-semibold text-white no-underline transition border-2 border-[#cc66ff] bg-black/20 hover:bg-black/5 hover:border-white hover:text-white"
-            >
-              Send
-            </button>
-          </div>
-        </form>
       </div>
+      <hr className="h-px border-0 bg-gradient-to-r from-#0000000 via-[#cc66ff] to-#0000000" />
+      <form
+        onSubmit={onSubmitHandler}
+        className="flex w-full flex-col gap-4 rounded-b-xl bg-white/10 p-4 text-white"
+        ref={formRef}
+      >
+        <textarea
+          name="content"
+          aria-label="message content input"
+          className="p-2 rounded-lg text-lg bg-gradient-to-b from-[#25213C] to-[#1b1b2e]"
+          rows={2}
+          required
+        />
+        <div className="flex justify-center">
+          <button
+            type="submit"
+            className="rounded-full px-10 py-3 font-semibold text-white no-underline transition border-2 border-[#cc66ff] bg-black/20 hover:bg-black/5 hover:border-white hover:text-white"
+          >
+            Send
+          </button>
+        </div>
+      </form>
     </section>
   );
 };
