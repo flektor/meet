b79d5496a4d656366e44531eaa05cb5c9d3eb958
flektor <gdrosin@gmail.com>
2023-08-21 19:17:23 +0200

    updated toast component so that it handles the user's interaction

--- src/components/Toasts/index.tsx
+++ src/components/Toasts/index.tsx
@@ -2,11 +2,52 @@ import { type FunctionComponent } from "react";
 import Toast from "../InvitationToast";
 import React from "react";
 import { useStore } from "../../utils/store";
+import { addDynamicGroupInput, PusherMessage } from "~/types";
+import { useRouter } from "next/router";
+import { api } from "~/utils/api";
 
 const Toasts: FunctionComponent = () => {
+  const router = useRouter();
   const store = useStore();
 
-  function onAcceptInvitation(toastId: string) {
+  const addDynamicGroup = api.groups.addDynamicGroup.useMutation({
+    onError: (error) => {
+      console.log({ error });
+    },
+    onSuccess: (group) => {
+      if (!group.slug) return;
+      router.push(`/activities/${group.activity}/${group.slug}`);
+    },
+  });
+
+  function onAcceptInvitation(toastId: string, message: PusherMessage) {
+    if (message.action !== "invite_accepted") {
+      return;
+    }
+
+    const activity = store.activities.find((activity) =>
+      activity.slug === message.activity
+    );
+
+    store.removeToast(message.requestId);
+    router.push(`/activities/${message.activity}/${message.group}`);
+
+    if (!activity) {
+      return;
+    }
+
+    try {
+      const data = addDynamicGroupInput.parse({
+        title: message.activity,
+        description: activity.description,
+        activityId: activity.id,
+        otherUserId: message.sentBy,
+        activitySlug: activity.slug,
+      });
+
+      addDynamicGroup.mutate(data);
+    } catch (error) {}
+
     store.removeToast(toastId);
   }
 
@@ -16,11 +57,11 @@ const Toasts: FunctionComponent = () => {
 
   return (
     <ul className="fixed top-20 right-3 z-20 flex flex-col gap-2">
-      {store.toasts.map(({ message, id }) => (
+      {store.toasts.map(({ pusherMessage, displayMessage, id }) => (
         <li key={id}>
           <Toast
-            message={message}
-            onAccept={() => onAcceptInvitation(id)}
+            message={displayMessage}
+            onAccept={() => onAcceptInvitation(id, pusherMessage)}
             onDecline={() => onDeclineInvitation(id)}
             onDie={() => store.removeToast(id)}
           />
