23dd23c7d183a15e6136042746814ed14b0bf7a3
flektor <gdrosin@gmail.com>
2023-10-05 23:48:09 +0200

    cleaned tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,22 +1,20 @@
-import { type Config } from "tailwindcss";
-
+/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/tw-elements/dist/js/**/*.js",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/tw-elements/dist/js/**/*.js",
   ],
 
   theme: {
+    darkMode: "class",
+
     extend: {
       colors: {
         primary: "#cc66ff",
       },
     },
   },
-  darkMode: "class",
-  plugins: [require("tw-elements/dist/plugin.cjs")],
+  plugins: [],
 };
