0890e16a5bad093f40bcacdfe6d1b9cfac8a4f6c
flektor <gdrosin@gmail.com>
2023-10-08 23:40:15 +0200

    moved toasts

--- src/components/Toasts/index.tsx
+++ src/components/Toasts/index.tsx
@@ -1,11 +1,10 @@
 import { type FunctionComponent } from "react";
-import Toast from "../Toast/InvitationToast";
+import Toast from "./Toast/InvitationToast";
 import React from "react";
 import { useStore } from "../../utils/store";
 
 const Toasts: FunctionComponent = () => {
   const store = useStore();
-
   return (
     <ul className="fixed top-20 right-3 z-20 flex flex-col gap-2">
       {store.toasts.map(({ displayMessage, pusherMessage, icon, ...rest }) => (
