34d9f79f6b802000f0b54760d6720f77c15156c7
flektor <gdrosin@gmail.com>
2023-10-14 17:53:47 +0200

    handling accept-invite case

--- src/utils/pusher/getToastId.ts
+++ src/utils/pusher/getToastId.ts
@@ -6,5 +6,10 @@ export default function getToastId(
   if (message.action === "quick_invite_request") {
     return `${message.activitySlug}:${message.sentBy}`;
   }
+
+  if (message.action === "invite_accepted") {
+    return `general.${message.groupId}:${message.sentBy}`;
+  }
+
   return `${message.activitySlug}.${message.groupSlug}:${message.sentBy}`;
 }
