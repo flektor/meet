a6e12a81bb2d6c8d17468b81c5d15f73c38e3877
flektor <gdrosin@gmail.com>
2023-10-16 00:52:36 +0200

    changed spinner size

--- src/components/Spinner/index.tsx
+++ src/components/Spinner/index.tsx
@@ -6,7 +6,7 @@ export default function Spinner() {
       <div role="status">
         <svg
           aria-hidden="true"
-          className="inline w-8 h-8 mr-2 text-cc66ff animate-spin dark:text-gray-600 fill-white"
+          className="inline w-6 h-6 mr-2 text-cc66ff animate-spin dark:text-gray-600 fill-white"
           viewBox="0 0 100 101"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
